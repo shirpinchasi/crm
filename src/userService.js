f7736bf4bd0bbde72c5f0f0e20eb216fa5b038d8
Shir Pinchasi <shirpi9999@gmail.com>
2022-08-05 15:01:37 +0300

    deleted unused code

--- src/userService.js
+++ src/userService.js
@@ -1,30 +1,6 @@
 import React, { useEffect, useState } from "react";
 import config from "./config/index"
 
-// export default function UserService() {
-// const [user, getUser] = useState([])
-
-//     async function get(){
-//         try{
-//             const res = await fetch(config.apiUrl + `/user/me`, {
-//                 method :"GET",
-//                 credentials:"include"
-//             });
-//             if (res.status === 403){
-//                 console.log("status 403")
-//                  return null;
-//             }
-//             const user = await res.json();
-//             getUser(user)
-//             console.log(user);
-//             return user;
-//          }catch(e){
-//              return null;
-//          }
-//     }
-//     }
-
-
 export class UserService{
     static async get(){
         try{
